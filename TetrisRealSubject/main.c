e26c4f9b3e2e543379936ffaf10e17c09675dd8b
Kim Seung Wook <kksy0316@gmail.com>
2024-12-02 19:38:10 +0900

    Master Branch Second commit
    main.c 소스 파일에 코드 작성

---
+++ TetrisRealSubject/main.c
@@ -0,0 +1,13 @@
+#include"Blocks.h"
+#include"Controller.h"
+#include"Screen.h"
+#include"ConsoleCursor.h"
+
+void main() {
+	CursorView(false);
+	system("mode con cols=76 lines=28 | title 창의실무프로젝트_테트리스 실습");
+	while (true) {
+		GameTitle();
+		GameProcess();
+	}
+}
